49eb0836afa38fce2d0cc1e756b360769ecbebd8
Harry Boyd <hboyd255@gmail.com>
2024-08-26 18:24:37 +0100

    Imported todoist methods from other project

---
+++ main.py
@@ -0,0 +1,35 @@
+from todoist_api_python.api import TodoistAPI
+from modules.file_utils import text_file_to_string, save_object, load_object
+
+
+# If set to True, the data will be loaded from the pickle files.
+# If set to False, the data will be loaded from the Todoist API.
+# This allows for faster testing.
+LOAD_FROM_PICKLE = False
+
+# The API key is stored in a text file to keep it secret from the git
+# repository.
+API_TEXT_FILE = "secrets/api_key.txt"
+API_KEY = text_file_to_string(API_TEXT_FILE)
+
+# Create an instance of the Todoist API.
+api = TodoistAPI(API_KEY)
+
+
+if LOAD_FROM_PICKLE:
+    all_tasks = load_object("secrets/tasks.pickle")
+    all_labels = load_object("secrets/labels.pickle")
+    all_projects = load_object("secrets/projects.pickle")
+
+else:
+    all_tasks = api.get_tasks()
+    all_labels = api.get_labels()
+    all_projects = api.get_projects()
+
+    save_object(all_tasks, "secrets/tasks.pickle")
+    save_object(all_labels, "secrets/labels.pickle")
+    save_object(all_projects, "secrets/projects.pickle")
+
+for project in all_projects:
+    print(project.name)
+    print()
